8c5343b1d5fb70ceaf38f05a61b7fe3bd25caa58
eche-unicorn <d.anasteysha@gmail.com>
2022-07-06 10:33:48 +0300

    table

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,54 @@
 import refs from "./refs.js";
 
-const { form, select } = refs;
-
-console.log(form);
+const { form, select, tableHead, dropdownList, currentLanguage, table } = refs;
+console.log(tableHead);
+let deliveryOptions = [];
+let tableOptions = [];
+let chosenCountry = "";
+console.log(chosenCountry);
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
+  document.querySelector("tbody")?.remove();
   console.log(e.target.elements);
   const data = new FormData(e.target);
 
   const request = Object.fromEntries(data.entries());
 
   console.log({ request });
+  const {
+    country,
+    weight,
+    weightUnit,
+    height,
+    length,
+    width,
+    measurementUnit,
+  } = request;
+  const arrangedData = { country, weight, rate: 1 };
+  chosenCountry = country;
+  const price = await sendData(arrangedData);
+  console.log(price);
+  buildTable(deliveryOptions, price);
+  table.classList.add("show-table");
+}
+
+async function sendData(data) {
+  const response = await fetch(`${BASE_URL}/public/delivery_rate/calc`, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "content-Type": "application/json",
+      owner: "5",
+    },
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  const json = await response.json();
+  console.log(json);
+
+  return json;
 }
 
 const BASE_URL = "https://logistic.ndv.net.ua/api";
@@ -24,6 +60,7 @@ function fetchCountries() {
     .then((response) => {
       if (response.ok) {
         console.log(response);
+
         return response.json();
       }
     })
@@ -33,7 +70,8 @@ function fetchCountries() {
 async function createList() {
   const countries = await fetchCountries();
   console.log(countries);
-
+  deliveryOptions = countries;
+  console.log("options", deliveryOptions);
   for (const country of countries) {
     const option = document.createElement("option");
     option.value = country.countryTo;
@@ -42,14 +80,43 @@ async function createList() {
   }
 }
 
+function buildTable(deliveryOptions, deliveryPrice) {
+  const getList = deliveryOptions.find(
+    (item) => item.countryTo === chosenCountry
+  );
+  console.log("list", getList);
+  getList.deliveryTypes.map(({ title, deliveryTime }) =>
+    tableOptions.push({ title, deliveryTime })
+  );
+  const body = document.createElement("tbody");
+
+  const items = tableOptions.map((option) => {
+    const row = document.createElement("tr");
+    const { title, deliveryTime } = option;
+    const method = document.createElement("td");
+    const time = document.createElement("td");
+    const price = document.createElement("td");
+    method.innerText = title;
+    time.innerText = deliveryTime;
+    price.innerText = deliveryPrice.price;
+    row.appendChild(method);
+    row.appendChild(time);
+    row.appendChild(price);
+
+    body.appendChild(row);
+    console.log(body);
+    return row;
+  });
+
+  tableHead.append(...items);
+}
+
 createList();
 select.addEventListener("change", function () {
   select.value = this.value;
+
   console.log("You selected: ", select.value);
 });
-
-const dropdownList = document.querySelector(".languages-dropdown");
-const currentLanguage = document.querySelector(".current-language");
 
 currentLanguage.addEventListener("mouseover", showDropdown);
 currentLanguage.addEventListener("blur", hideDropdown);
--- js/refs.js
+++ js/refs.js
@@ -1,4 +1,8 @@
 export default {
   form: document.querySelector("#js-form"),
   select: document.getElementById("countries-select"),
+  table: document.querySelector(".calculator-table"),
+  tableHead: document.querySelector(".tableHead"),
+  dropdownList: document.querySelector(".languages-dropdown"),
+  currentLanguage: document.querySelector(".current-language"),
 };
