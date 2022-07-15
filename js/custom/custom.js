182824d806d9a6d2054c1dfc82f94cff70e8a1f2
eche-unicorn <d.anasteysha@gmail.com>
2022-07-15 18:15:38 +0300

    fix

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -58,7 +58,7 @@ async function handleSubmit(e) {
   const getPrices = dataSets.map((item) =>
     sendData(item)
       .then((response) => {
-        // console.log(response);
+        console.log(response);
         let result;
         if (!response.message) {
           result = {
@@ -135,12 +135,12 @@ select.addEventListener("change", function () {
   select.value = this.value;
 });
 form.addEventListener("submit", handleSubmit);
-currentLanguage.addEventListener("mouseover", showDropdown);
-currentLanguage.addEventListener("blur", hideDropdown);
-document
-  .querySelector(".languages-dropdown")
-  .addEventListener("mouseout", hideDropdown);
-overlay.addEventListener("click", hideDropdown);
+// currentLanguage.addEventListener("mouseover", showDropdown);
+// currentLanguage.addEventListener("blur", hideDropdown);
+// currentLanguage.addEventListener("mouseout", hideDropdown);
+// overlay.addEventListener("mouseover", hideDropdown);
+
+// overlay.addEventListener("click", hideDropdown);
 resetForm.addEventListener("click", function () {
   calcResult.innerHTML = "";
   tableOptions = [];
