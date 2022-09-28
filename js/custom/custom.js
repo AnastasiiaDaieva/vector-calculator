2b5ab9aa2f0cf9f282bf77051329673d112bad08
eche-unicorn <d.anasteysha@gmail.com>
2022-09-28 16:06:28 +0300

    check bg

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -170,7 +170,7 @@ resetForm.addEventListener("click", function () {
   form.reset();
 });
 
-getColor(ownerParam, "main");
+getColor(ownerParam, "shipment");
 
 document.addEventListener("DOMContentLoaded", () => {
   setLocale(ownerParam, languageParam);
