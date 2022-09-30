2a3463495dd15906f7946b3bed3ae35c457fe247
eche-unicorn <d.anasteysha@gmail.com>
2022-09-30 11:30:59 +0300

    base changed

--- js/custom/getColor.js
+++ js/custom/getColor.js
@@ -2,7 +2,7 @@ import { BASE_URL } from "./variables.js";
 
 export async function getColor(websiteId, coloredElementClass) {
   try {
-    const data = await fetch(`${BASE_URL}/public/websites/${websiteId}`);
+    const data = await fetch(`${BASE_URL}/websites/${websiteId}`);
     const json = await data.json();
 
     document.querySelector(`.${coloredElementClass}`).style.backgroundColor =
