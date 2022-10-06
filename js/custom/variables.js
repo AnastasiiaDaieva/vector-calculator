42e2b59096fe98ed31be93b875b5eb63fec867f3
eche-unicorn <d.anasteysha@gmail.com>
2022-10-06 21:44:22 +0300

    base changed

--- js/custom/variables.js
+++ js/custom/variables.js
@@ -1,6 +1,6 @@
 const test = "https://logistic.ndv.net.ua";
 const real = "https://next.crauzer.com";
 
-export const BASE_URL = test;
+export const BASE_URL = real;
 export const URL_API = "/api";
 export const BASE_WITH_API = `${BASE_URL}${URL_API}`;
